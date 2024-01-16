b46a0cee2fb28625fc5135b509d25bfc31f08901
Pierre Vincent <pvincent24@students.polytechnic.org>
2024-01-16 10:26:23 -0800

    Preliminary work on Song.java

---
+++ Song.java
@@ -0,0 +1,63 @@
+/**
+ * File for a Song class to be used in the Playlist Project
+ * @author Pierre
+ * @version 1/16/23
+ */
+public class Song {
+    //Fields-- what information do we want each Song to store?
+
+    private String name;
+    private String artist;
+    private int duration;
+
+public 
+
+
+    /**
+     * Constructor-- what information needs to be given to make a Song?
+     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+     */
+    public Song(String name, String artist, string duration)
+    {
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+
+
+
+     /**
+      * Methods-- what will you want each Song to do?
+      * Consider all the getter methods (getName, getArtist, etc.)
+      * You should probably have a toString method to be able to get the information for the full
+      * song easily as well!
+      * What kind of mutator (setter) methods will you need?
+      */
+    public void add()
+    
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public double getDuration()
+    {
+        return duration;
+    }
+
+    public boolean isLiked()
+    {
+        return liked;
+    }
+
+    public void like()
+    {
+        
+
+}
